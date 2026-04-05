c5d30f4b174bcec12f8d9a801466beda0eb5dde7
singhabhi03 <abhishek.nsec04@gmail.com>
2026-04-05 00:15:29 -0400

    DataTypes

---
+++ Day2/PrimitiveDataTypes.js
@@ -0,0 +1,12 @@
+console.log("JMS Randhir Singh")
+firstname="Abhishek Singh"
+age =35
+isMarried = true
+//lastname;
+lastname =undefined;
+webdriver = null;
+console.log(typeof(firstname))
+console.log(typeof(age))
+console.log(typeof(isMarried))
+console.log(typeof(lastname))
+console.log(typeof(webdriver)) //object -->bug in JS
