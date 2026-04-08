c848f64843d4ccfd8dc03b85de7253a351e35374
singhabhi04 <abhishek.topa@gmail.com>
2026-04-07 23:43:23 -0400

    String Basics

---
+++ StringInJS/TemplateString.js
@@ -0,0 +1,14 @@
+let fname ="Abhishek"
+let lname ="Singh"
+let city ='Ramgarh'
+let profession ='Quality Engineer'
+
+console.log("My Name is "+fname+" "+lname)
+let userDetails = `My Name is ${fname} ${lname} and I am from ${city} and I work as an ${profession}`
+console.log(userDetails)
+// Multi Line String
+
+let aboutYourself =`I am from a village in Jharkhand called Rabodh
+I have two sisters and no Brother`
+
+console.log(aboutYourself)
